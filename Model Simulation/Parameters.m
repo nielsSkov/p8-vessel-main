de25196bca6a12b164070e1fb5173271d7c253ff
Noelia <noelia.va.3003@gmail.com>
2017-02-07 09:42:10 +0100

    Add simulation of the model

---
+++ Model Simulation/Parameters.m
@@ -0,0 +1,22 @@
+close all
+clear
+clc
+
+% Parameters
+mx=200;     % [kg] mass + added mass in xb direction
+my=250;     % [kg] mass + added mass in yb direction
+Iz=700;     % [kgm2]
+
+dx=70;      % [kg/s] 
+dy=100;     % [kg/s]
+dyaw=50;    % [kgm2/s]
+
+l1=1;
+l2=1;
+l3=2;
+l4=2;
+
+F1=0;
+F2=200;
+F3=0;
+F4=0;
