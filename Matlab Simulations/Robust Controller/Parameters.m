cf911bf8ccbfa6b33eb220bbff9ee965b52c6bb6
Noelia <noelia.va.3003@gmail.com>
2017-03-13 16:51:21 +0100

    Add matlab file with start of robust control

---
+++ Matlab Simulations/Robust Controller/Parameters.m
@@ -0,0 +1,25 @@
+close all
+clear
+clc
+
+% Parameters
+mx=13;     % [kg]
+my=13;     % [kg]
+mz=13;     % [kg]
+Ix=0.06541;     % [kgm2]
+Iy=1.08921;     % [kgm2]
+Iz=1.10675;     % [kgm2]
+
+dx=2.86;    % [] 
+dy=32.5;	% []
+dz=0;    % []
+droll=0.1094;  % []
+dpitch=7.203; % []
+dyaw=0.26285;    % []
+
+l1=0.05;   % [m]
+l2=0.05;   % [m]
+
+Troll=6.9736;   % [N/m]
+Tpitch=131.8316;   % [N/m]
+
