729d8031f8c4e6acffe2a9cdb816bd4ec6cda3a9
hkooverjee <hkooverjee@gmail.com>
2017-04-12 16:04:24 +0200

    Updated robustModel.m

--- Model Simulation/robustModel.m
+++ Model Simulation/robustModel.m
@@ -5,10 +5,17 @@ clc
 
 run Parameters
 
+
+%% Varying Parameters
 m=m-2.6;
 Iz=Iz-0.22;
-%%
-% State Space Model - states=[yaw yaw_dot xb_dot]'
+
+% Reference signals for simulink
+refYaw = 1;      % [rad]
+refXdot = 1;        % [m/s]
+
+%% State Space Model
+% states=[yaw yaw_dot xb_dot]'
 A=[0 1 0;
     0 -dyaw/Iz 0;
     0 0 -dx/m];
@@ -20,65 +27,113 @@ C=[1 0 0;
 D=[0 0;
     0 0];
 
-refYaw = 1;      % [rad]
-refXdot = 1;        % [m/s]
+nx = size(A,2);         % number of states
+nu = size(B,2);         % number of controlled inputs
+ny = size(C,1);         % number of measured outputs
 
-nx = size(A,2);
-nu = size(B,2);
-ny = size(C,1);
+% Cz is C matrix for performance output (Z) equation (Weightings)
+% For states
+Cz = [1 0 0;
+    0 0.5 0;
+    0 0 0.5];
+
+% Dz is D matrix for performance output (Z) equation (Weightings)
+% For inputs
+Dz = [0.001 0;
+    0 0.001];
+
 nz = ny + nu;
-nd = nu;    % number of input disturbance states
 nw = ny;    % number of output noise states
 
+%% Integral Model
 
-%% Integral Action
-% Extended ss model with integral states
+ni = ny;                % number of reference (integral) signals to track
 
-Ai = [A zeros(nx,ny);
-    -C -0.001*eye(ny,ny)];
-Bi = [B; 
-    zeros(ny,nu)];
-Ci = [C zeros(ny,ny)];
-Di = D;
-%% Hinfinity Model with disturbances
+Ai = -0.001*eye(ni);
+Bi = eye(ni);
 
-A1=[Ai zeros(nx+ny,nd);
-    zeros(nd,nx+ny) -50*eye(nd,nd)];
+% C matrix for performance output (Z) equation (Weightings)
+% For integral states
+Ci = [1 0;
+    0 1];
 
+%% Disturbance Model
+
+nd = nu;    % number of input disturbance states
+
+Ad = -50*eye(nd);
+Bd = 50*eye(nd);
+
+% Matrix describing how disturbance forces affect states
 Bdist=[0 0;
     1/Iz 0;
     0 1/m];
 
-B1 = [Bdist zeros(nx,nw) zeros(nx,ny);
-    zeros(ny,nd) zeros(ny,nw) eye(ny,ny)
-    50*eye(nd,nd) zeros(ny,nw) zeros(nd,ny)];
+% C matrix for performance output (Z) equation (Weightings)
+% For input disturbance states
+Cd = [1 0;
+    0 1];
+%% Noise Model
 
-B2 = [Bi;
-    zeros(nd,nd)];
+nn = ny;    % number of output measurement noise states
 
-C1 = [diag([1,0.5,0.5,1,1,1,1]);   %Weighting on the states
-    zeros(nu,nx+ny+nd)];
+An = -200*eye(nn);
+Bn = 00*eye(nn);
 
-D11 = [zeros(nx,nd) zeros(nx, nw) zeros(nx, ny);
-    zeros(ny, nd) zeros(ny, nw) zeros(ny, ny);
-    zeros(nd, nd) zeros(nd, nw) zeros(nd, ny);
-    zeros(nu, nd) zeros(nu, nw) zeros(nu, ny)];
+% C matrix for performance output (Z) equation (Weightings)
+% For output measurement noise states
+Cn = [1 0;
+    0 1];
 
-D12 = [zeros(nx+ny+nd,nu);  %Weighting on the inputs
-     0.001*eye(nu,nu)];
+%% H infinity Model
+% x' = A1x + B1w  + B2u     - state equation
+% z  = C1x + D11w + D12u    - performance output equation
+% y  = C2x + D21w + D22u    - measured output equation
+%
+% x = [x xi xd xn]'         - [states int_states dist_states noise_states]'
+% w = [r d n]'              - uncontrollable inputs
+% u = u                     - controllable inputs
+% z = [x xi xd xn u]'       - weightings / performance outputs
+% y = [y r]                 - measured ouputs
 
-C2 = [Ci zeros(ny,nd);
-    zeros(ny,nx) eye(ny) zeros(ny,nd)];
+A1 = [A zeros(nx,ni+nd+nn);             % states
+    -C Ai zeros(ni,nd+nn);              % integral states 'xi = -Cx+r'
+    zeros(nd,nx+ni) Ad zeros(nd,nn);    % disturbance states
+    zeros(nn,nx+ni+nd) An];             % noise states
 
-D21 = [zeros(ny,nd) eye(ny, nw) zeros(ny,ny);
-    zeros(ny,nd) zeros(ny, nw) zeros(ny,ny)];
+B1 = [zeros(nx,ni) Bdist zeros(nx,nn);  % effect of uncont inputs on states
+    eye(ni,ni) zeros(ni,nd+nn);         % adding ref to int_states
+    zeros(nd,ni) Bd zeros(nd,nn);       % see disturbance model
+    zeros(nn,ni+nd) Bn];                % see noise model
 
-D22 = zeros(2*ny,nu);
+B2 = [B;
+    zeros(ni+nd+nn,nu)];
+
+C1 = [Cz zeros(nx,ni+nd+nn);
+    zeros(ni,nx) Ci zeros(ni,nd+nn);
+    zeros(nd,nx+ni) Cd zeros(nd,nn);
+    zeros(nn,nx+ni+nd) Cn;
+    zeros(nu,nx+ni+nd+nn)];
+
+D11 = zeros(nx+ni+nd+nn+nu,ni+nd+nn);
+
+D12 = [zeros(nx+ni+nd+nn,nu);
+    Dz];
+
+C2 = [C zeros(ny,ni+nd+nn);
+    zeros(ni,nx) eye(ni) zeros(ni,nd+nn)];
+
+D21 = [zeros(ny,ni+nd) eye(nn);
+    zeros(ni,ni+nd+nn)];
+
+D22 = zeros(ny+ni,nu);
 
 N = [A1 B1 B2;
     C1 D11 D12;
     C2 D21 D22];
 
+%% H infinity controller design
+    
 Ah = A1;
 
 Bh = [B1 B2];
@@ -93,172 +148,7 @@ sysN = ss(Ah,Bh,Ch,Dh);
 [k,g,gfin, info] = hinfsyn(sysN,2*ny,nu,'GMIN',1,'GMAX',5,'DISPLAY','on','TOLGAM',0.01);
 %info.GAMFI
 
-F = -info.KFI(1:2,1:3)
-FI= -info.KFI(1:2,4:5)
-Fd=-info.KFI(1:2,6:7);
+F  = -info.KFI(1:2,1:3);git 
+FI = -info.KFI(1:2,4:5);
+Fd = -info.KFI(1:2,6:7);
 
-%% Hinfinity Model
-
-Bdist=[0 0;
-    1/Iz 0;
-    0 1/m];
-
-B1 = [0.1*Bdist zeros(nx,nw) zeros(nx,ny);
-    zeros(ny,nd) zeros(ny,nw) eye(ny,ny)];
-
-B2 = Bi;
-
-C1 = [diag([1,0.5,0.5,1,1]);   %Weighting on the states
-    zeros(nu,nx+ny)];
-
-D11 = [zeros(nx,nd) zeros(nx, nw) zeros(nx, ny);
-    zeros(ny, nd) zeros(ny, nw) zeros(ny, ny);
-    zeros(nu, nd) zeros(nu, nw) zeros(nu, ny)];
-
-D12 = [zeros(nx+ny,nu);  %Weighting on the inputs
-     0.001*eye(nu,nu)];
-
-C2 = [Ci;
-    zeros(ny,nx) eye(ny)];
-
-D21 = [zeros(ny,nd) 10*eye(ny, nw) zeros(ny,ny);
-    zeros(ny,nd) zeros(ny, nw) zeros(ny,ny)];
-
-D22 = zeros(2*ny,nu);
-
-N = [Ai B1 B2;
-    C1 D11 D12;
-    C2 D21 D22];
-
-Ah = Ai;
-
-Bh = [B1 B2];
-
-Ch = [C1;
-    C2];
-
-Dh = [D11 D12;
-    D21 D22];
-
-sysN = ss(Ah,Bh,Ch,Dh);
-[k,g,gfin, info] = hinfsyn(sysN,2*ny,nu,'GMIN',1,'GMAX',1.2);
-info.KFI
-
-F = info.KFI(1:2,1:3)
-FI= info.KFI(1:2,4:5)
-
-%% Hinfinity Model extended with the disturbance
-
-A1=[Ai zeros(nx+ny,nd);
-    zeros(nd,nx+ny+nd)];
-Bdist=[0 0;
-    1/Iz 0;
-    0 1/m];
-
-B1 = [0.1*Bdist zeros(nx,nw) zeros(nx,ny);
-    zeros(ny,nd) zeros(ny,nw) eye(ny,ny);
-    eye(nd,nd) zeros(nd,nw) zeros(nd,ny)];
-B2 = [Bi
-      zeros(nd,nu)];
-
-C1 = [diag([1,0.5,0.5,1,1,1,1]);   %Weighting on the states
-    zeros(nu,nx+ny+nd)];
-
-D11 = [zeros(nx,nd) zeros(nx, nw) zeros(nx, ny);
-    zeros(ny, nd) zeros(ny, nw) zeros(ny, ny);
-    zeros(nd, nd) zeros(nd, nw) zeros(nd, ny);
-    zeros(nu, nd) zeros(nu, nw) zeros(nu, ny)];
-
-D12 = [zeros(nx+ny+nd,nu);  %Weighting on the inputs
-     0.001*eye(nu,nu)];
-
-C2 = [Ci zeros(ny,nd);
-    zeros(ny,nx) eye(ny) zeros(ny,nd)];
-
-D21 = [zeros(ny,nd) ones(ny, nw) zeros(ny,ny);
-    zeros(ny,nd) zeros(ny, nw) zeros(ny,ny)];
-
-D22 = zeros(2*ny,nu);
-
-N = [A1 B1 B2;
-    C1 D11 D12;
-    C2 D21 D22];
-
-Ah = A1;
-
-Bh = [B1 B2];
-
-Ch = [C1;
-    C2];
-
-Dh = [D11 D12;
-    D21 D22];
-
-sysN = ss(Ah,Bh,Ch,Dh);
-[k,g,gfin, info] = hinfsyn(sysN,2*ny,nu,'GMAX',2,'GMIN',2)
-info.KFI
-
-F = info.KFI(1:2,1:3)
-FI= info.KFI(1:2,4:5)
-
-
-%% Hinf 2
-
-%A1 = A;
-
-%B1 = [Bdist zeros(nx,nw) zeros(nx,ny)];
-
-%B2 = B;
-
-%C1 = [Ci;
-%    zeros(nu,nx)];
-
-% D11 = [zeros(ny,nd) zeros(ny,nw) eye(ny,ny);
-%     zeros(nu,nd) zeros(nu,nw) zeros(nu,ny)];
-% 
-% D12 = [zeros(ny,nu);
-%     eye(nu,nu)];
-
-%C2 = C;
-
-%D21 = [zeros(ny,nd) eye(ny, nw) zeros(ny,ny)];
-
-%D22 = zeros(ny,nu);
-
-% N = [Ai B1 B2;
-%     C1 D11 D12;
-%     C2 D21 D22];
-% 
-% Ah = Ai;
-% 
-% Bh = [B1 B2];
-% 
-% Ch = [C1;
-%     C2];
-% 
-% Dh = [D11 D12;
-%     D21 D22];
-% 
-% sysN = ss(Ah,Bh,Ch,Dh);
-% [k,g,gfin, info] = hinfsyn(sysN,ny,nu);
-% info.KFI
-
-
-%%
-
-% [numG1, denG1] = ss2tf(A,B,C,D,1);
-% %[z,p] = tf2zp(numG1,denG1);
-% [numG2, denG2] = ss2tf(A,B,C,D,2);
-% %[z,p] = tf2zp(numG2,denG2);
-% 
-% 
-% G11 = tf(numG1(1,:), denG1);
-% G12 = tf(numG2(1,:), denG2);
-% G21 = tf(numG1(2,:), denG1);
-% G22 = tf(numG2(2,:), denG2);
-% 
-% G = [G11 G12;
-%     G21 G22];
-
-
-%[k,g,gfin, info] = hinfsyn(G,ny,nu);
